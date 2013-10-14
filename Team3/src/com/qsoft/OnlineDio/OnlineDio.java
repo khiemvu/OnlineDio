b298353cf9fa132be33e9aa5ba0feecad7214b87
unknown <khiemvx@qs098.qsoft.com.vn>
2013-10-14 08:48:42 +0700

    project OnlineDio

---
+++ Team3/src/com/qsoft/OnlineDio/OnlineDio.java
@@ -0,0 +1,17 @@
+package com.qsoft.OnlineDio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OnlineDio extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
